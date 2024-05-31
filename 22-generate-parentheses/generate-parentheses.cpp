68e92701559ea66151b0c50ca4cfb4bca1d76a30
Upasana Shakya <123869989+Upasana-Shakya@users.noreply.github.com>
2024-06-01 00:19:57 +0530

    Time: 5 ms (32.20%) | Memory: 15.6 MB (35.56%) - LeetSync

---
+++ 22-generate-parentheses/generate-parentheses.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    void solve(vector<string> &ans, int n, int open, int close, string output){
+        //Base Case
+        if(open == 0 && close == 0){
+            ans.push_back(output);
+            return;
+        }
+
+        // Include open bracket
+        if(open > 0){
+            output.push_back('(');
+            //recursion
+            solve(ans, n, open-1, close, output);
+            //backtrack
+            output.pop_back();
+        }
+
+        // Include close bracket
+        if(close > open){
+            output.push_back(')');
+            //recursion
+            solve(ans, n, open, close-1, output);
+            //backtrack
+            output.pop_back();
+        }
+
+    }
+    vector<string> generateParenthesis(int n) {
+        vector<string> ans;
+        string output = "";
+
+        int open = n;
+        int close = n;
+
+        solve(ans, n, open, close, output);
+
+        return ans;
+    }
+};
