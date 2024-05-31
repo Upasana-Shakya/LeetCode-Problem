cbac9319ccbea8a53a63172ac3860405201df9c3
Upasana Shakya <123869989+Upasana-Shakya@users.noreply.github.com>
2024-06-01 00:59:10 +0530

    Time: 11 ms (12.60%) | Memory: 11.3 MB (10.45%) - LeetSync

---
+++ 52-n-queens-ii/n-queens-ii.cpp
@@ -0,0 +1,68 @@
+class Solution {
+public:
+    unordered_map<int, bool> rowCheck;
+    unordered_map<int, bool> upperLeftDiagonalCheck;
+    unordered_map<int, bool> bottomLeftDiagonalCheck;
+
+    bool isSafe(int row, int col, vector<vector<char>> &board, int n){
+        if(rowCheck[row] == true){
+            return false;
+        }
+        if(upperLeftDiagonalCheck[col-row] == true){
+            return false;
+        }
+        if(bottomLeftDiagonalCheck[row+col] == true){
+            return false;
+        }
+        return true;
+    }
+
+    void storeSol(vector<vector<char>> &board, int n, vector<vector<string>> &ans){
+        vector<string> temp;
+        for(int i=0 ; i<n ; i++){
+            string output = "";
+            for(int j=0 ; j<n ; j++){
+                output.push_back(board[i][j]);
+            }
+            temp.push_back(output);
+        }
+        ans.push_back(temp);
+    }
+
+    void solve(vector<vector<char>> &board, int col, int n, vector<vector<string>> &ans){
+        //Base case
+        if(col >= n){
+            storeSol(board, n, ans);
+            return;
+        }
+
+        // 1 case solve karna hai baki recursion sambhal lega
+        for(int row = 0 ; row < n ; row++){
+            if(isSafe(row, col, board, n)){
+                // Rakh do
+                board[row][col] = 'Q';
+                rowCheck[row] = true;
+                upperLeftDiagonalCheck[col-row] = true;
+                bottomLeftDiagonalCheck[row+col] = true;
+
+                //Recursive soln
+                solve(board, col+1, n, ans);
+                
+                // Backtrack
+                board[row][col] = '.';
+                rowCheck[row] = false;
+                upperLeftDiagonalCheck[col-row] = false;
+                bottomLeftDiagonalCheck[row+col] = false;
+            }
+        }
+    }
+    int totalNQueens(int n) {
+        vector<vector<char>> board(n, vector<char>(n,'.'));
+        vector<vector<string>> ans;
+
+        int col = 0;
+        solve(board, col, n, ans);
+
+        return ans.size();
+    }
+};
