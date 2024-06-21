e2f03c2c4573d1524d908f22ab7d68f9a808b88a
Upasana Shakya <123869989+Upasana-Shakya@users.noreply.github.com>
2024-06-21 22:44:43 +0530

    Time: 3 ms (84.77%) | Memory: 22.7 MB (30.57%) - LeetSync

--- 3421-count-pairs-that-form-a-complete-day-i/count-pairs-that-form-a-complete-day-i.cpp
+++ 3421-count-pairs-that-form-a-complete-day-i/count-pairs-that-form-a-complete-day-i.cpp
@@ -4,7 +4,7 @@ public:
         int count = 0;
         int n = hours.size();
 
-        for(int i = 0 ; i<n ; i++){
+        for(int i = 0 ; i<n-1 ; i++){
             for(int j = i+1 ; j<n ; j++){
                 if((hours[i]+hours[j]) % 24 == 0){
                     count++;
