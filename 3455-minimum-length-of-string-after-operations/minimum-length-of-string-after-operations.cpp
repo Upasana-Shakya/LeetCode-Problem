df19309678f8d8510d62c554c6f48bd2c87caf0e
Upasana Shakya <123869989+Upasana-Shakya@users.noreply.github.com>
2024-07-21 13:54:31 +0530

    Time: 92 ms (50.00%) | Memory: 30.6 MB (50.00%) - LeetSync

---
+++ 3455-minimum-length-of-string-after-operations/minimum-length-of-string-after-operations.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int minimumLength(string s) {
+        vector<int> freq(26, 0);
+        int ans = 0;
+
+        for(int i=0 ; i<s.size() ; i++){
+            freq[s[i]-'a']++;
+        }
+        for(int i=0 ; i<26 ; i++){
+            if(freq[i] < 3){
+                ans += freq[i];
+            }
+            else{
+                if(freq[i]%2 == 0){
+                    ans += 2;
+                }
+                else{
+                    ans += 1;
+                }
+            }
+        }
+        return ans;
+    }
+};
