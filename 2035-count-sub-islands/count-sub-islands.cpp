760c22cb56a43805bcf9f026f6387f2db7758447
Upasana Shakya <123869989+Upasana-Shakya@users.noreply.github.com>
2024-08-28 11:08:12 +0530

    Time: 198 ms (90.35%) | Memory: 95.7 MB (60.76%) - LeetSync

--- 2035-count-sub-islands/count-sub-islands.cpp
+++ 2035-count-sub-islands/count-sub-islands.cpp
@@ -1,8 +1,10 @@
 class Solution {
 public:
-    void solve(int i, int j, vector<vector<int>>& grid1, vector<vector<int>>& grid2, int &flag, int m, int n){
+    void solve(int i, int j, vector<vector<int>>& grid1, vector<vector<int>>& grid2, bool &flag, int m, int n){
         if(i<0 || j<0 || i >=m || j >= n || grid2[i][j] == 0) return;
-        if(grid1[i][j] == 0) flag = 0;
+        if(grid1[i][j] == 0){
+            flag = 0;
+        }
         grid2[i][j] = 0;
 
         solve(i-1, j, grid1, grid2, flag, m, n);
@@ -19,9 +21,9 @@ public:
         for(int i=0 ; i<m ; i++){
             for(int j=0 ; j<n ; j++){
                 if(grid2[i][j] == 1){
-                    int flag = 1;
+                    bool flag = 1;
                     solve(i, j, grid1, grid2, flag, m, n);
-                    res += flag;
+                    if(flag) res++;
                 }
             }
         }
