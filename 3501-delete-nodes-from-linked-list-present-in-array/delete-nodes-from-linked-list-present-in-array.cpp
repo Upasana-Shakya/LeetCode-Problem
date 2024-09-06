2d9a48fa1c3f736aa53e21aaa22bf511946da209
Upasana Shakya <123869989+Upasana-Shakya@users.noreply.github.com>
2024-09-06 16:14:50 +0530

    Time: 439 ms (61.29%) | Memory: 262.7 MB (46.88%) - LeetSync

---
+++ 3501-delete-nodes-from-linked-list-present-in-array/delete-nodes-from-linked-list-present-in-array.cpp
@@ -0,0 +1,32 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* modifiedList(vector<int>& nums, ListNode* head) {
+        unordered_set<int> numsSet(nums.begin(), nums.end());
+        ListNode* prev = new ListNode(0);
+        prev->next = head;
+        ListNode* curr = prev;
+        
+        while(curr->next != NULL){
+            if(numsSet.count(curr->next->val) > 0){
+                ListNode* temp = curr->next;
+                curr->next = curr->next->next;
+                delete temp;
+            }
+            else{
+                curr = curr->next;
+            }
+        }
+        
+        return prev->next;
+    }
+};
