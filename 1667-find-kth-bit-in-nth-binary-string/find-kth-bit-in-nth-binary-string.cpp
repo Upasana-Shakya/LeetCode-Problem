608091cccdc967fc2ad506f580b569ea76230435
Upasana Shakya <123869989+Upasana-Shakya@users.noreply.github.com>
2024-10-19 22:57:56 +0530

    Time: 47 ms (25.94%) | Memory: 58.8 MB (40.45%) - LeetSync

---
+++ 1667-find-kth-bit-in-nth-binary-string/find-kth-bit-in-nth-binary-string.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    string invert(string s){
+        int n = s.size();
+
+        for(int i=0 ; i<n ; i++){
+            if(s[i] == '0'){
+                s[i] = '1';
+            }
+            else{
+                s[i] = '0';
+            }
+        }
+        return s;
+    }
+    char findKthBit(int n, int k) {
+        vector<string> vs(n);
+
+        vs[0] = "0";
+
+        for(int i=1 ; i<n ; i++){
+            string s = invert(vs[i-1]);
+            reverse(s.begin(), s.end());
+            vs[i] = vs[i-1] + "1" + s;
+        }
+
+        return vs[n-1][k-1];
+    }
+};
