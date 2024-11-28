47f5953eb963937c0e3958d8197d9b20a209c541
Upasana Shakya <123869989+Upasana-Shakya@users.noreply.github.com>
2024-11-28 22:53:53 +0530

    Time: 679 ms (29.80%) | Memory: 0B (100.00%) - LeetSync

--- 183-customers-who-never-order/customers-who-never-order.sql
+++ 183-customers-who-never-order/customers-who-never-order.sql
@@ -1,5 +1,3 @@
 # Write your MySQL query statement below
 SELECT customers.name as Customers FROM customers 
-WHERE id NOT IN (SELECT c.id FROM customers as c
-JOIN orders AS o
-ON c.id = o.customerId);
+WHERE id NOT IN (SELECT customerId FROM orders);
