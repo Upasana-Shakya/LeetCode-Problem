5b0ee985c587fb5e48b07fb2a35abb44474850cd
Upasana Shakya <123869989+Upasana-Shakya@users.noreply.github.com>
2024-12-04 10:34:33 +0530

    Time: 4 ms (51.93%) | Memory: 17.1 MB (7.53%) - LeetSync

--- 3018-make-string-a-subsequence-using-cyclic-increments/make-string-a-subsequence-using-cyclic-increments.cpp
+++ 3018-make-string-a-subsequence-using-cyclic-increments/make-string-a-subsequence-using-cyclic-increments.cpp
@@ -7,11 +7,7 @@ public:
         int j=0;
 
         while(i < n1 && j < n2){
-            if(str1[i] == str2[j] || str1[i]+1 == str2[j]){
-                i++;
-                j++;
-            }
-            else if(str1[i] == 'z' && str2[j] == 'a'){
+            if(str1[i] == str2[j] || str1[i]+1 == str2[j] || (str1[i] == 'z' && str2[j] == 'a')){
                 i++;
                 j++;
             }
