6ce14a7a9d2b24a587ee734d06fa665d53ccec63
Upasana Shakya <123869989+Upasana-Shakya@users.noreply.github.com>
2025-02-01 22:17:00 +0530

    Time: 0 ms (100.00%) | Memory: 27.8 MB (25.00%) - LeetSync

--- 3429-special-array-i/special-array-i.cpp
+++ 3429-special-array-i/special-array-i.cpp
@@ -3,20 +3,8 @@ public:
     bool isArraySpecial(vector<int>& nums) {
         int n = nums.size();
 
-        if(n == 1){
-            return true;
-        }
-        else if(n == 2){
-            if(nums[0]%2 == nums[1]%2){
-                return false;
-            }
-            else{
-                return true;
-            }
-        }
-
-        for(int i=1 ; i<n-1 ; i++){
-            if((nums[i-1]%2) == (nums[i]%2) || (nums[i]%2) == (nums[i+1]%2)) {
+        for(int i=0 ; i<n-1 ; i++){
+            if(nums[i]%2 == nums[i+1]%2){
                 return false;
             }
         }
