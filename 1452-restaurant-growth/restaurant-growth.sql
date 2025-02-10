08717c5e572c01b70a028df377575deb364d3c05
Upasana Shakya <123869989+Upasana-Shakya@users.noreply.github.com>
2025-02-11 02:25:32 +0530

    Time: 563 ms (67.25%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1452-restaurant-growth/restaurant-growth.sql
@@ -0,0 +1,11 @@
+# Write your MySQL query statement below
+# Write your MySQL query statement below
+SELECT distinct visited_on,
+    (SELECT SUM(amount)
+        FROM Customer as c1
+        WHERE DATE_SUB(c.visited_on, INTERVAL 6 DAY) <= c1.visited_on AND c1.visited_on <= c.visited_on) as amount,
+    (SELECT ROUND(SUM(amount)/7,2)
+        FROM Customer as c1
+        WHERE DATE_SUB(c.visited_on, INTERVAL 6 DAY) <= c1.visited_on AND c1.visited_on <= c.visited_on) as average_amount
+FROM Customer c
+WHERE DATE_SUB(visited_on, INTERVAL 6 DAY) >= (SELECT MIN(visited_on) FROM Customer)
