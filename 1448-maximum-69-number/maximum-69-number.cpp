397141c4a32757ba3a81327a7ed1786e1d543512
Upasana Shakya <123869989+Upasana-Shakya@users.noreply.github.com>
2025-08-16 12:32:36 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (71.54%) - LeetSync

--- 1448-maximum-69-number/maximum-69-number.cpp
+++ 1448-maximum-69-number/maximum-69-number.cpp
@@ -1,27 +1,14 @@
 class Solution {
 public:
     int maximum69Number (int num) {
-        vector<int> temp;
+        string s = to_string(num);
 
-        while(num){
-            temp.push_back(num%10);
-            num /= 10;
-        }
-
-        reverse(temp.begin(), temp.end());
-
-        for(int i=0 ; i<temp.size() ; i++){
-            if(temp[i] == 6){
-                temp[i] = 9;
+        for(auto &ch : s){
+            if(ch == '6'){
+                ch = '9';
                 break;
             }
         }
-
-        int ans = 0;
-
-        for(auto i : temp){
-            ans = ans * 10 + i;
-        }
-        return ans;
+        return stoi(s);
     }
 };
