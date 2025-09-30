92fef343c8a6d1b313cb6c07a8fa61377efef0c9
Upasana Shakya <123869989+Upasana-Shakya@users.noreply.github.com>
2025-09-30 23:36:14 +0530

    Time: 136 ms (42.43%) | Memory: 144.4 MB (32.44%) - LeetSync

--- 2324-find-triangular-sum-of-an-array/find-triangular-sum-of-an-array.cpp
+++ 2324-find-triangular-sum-of-an-array/find-triangular-sum-of-an-array.cpp
@@ -1,24 +1,40 @@
 class Solution {
 public:
     int triangularSum(vector<int>& nums) {
-        if(nums.size() == 1){
+
+        // BRUTE FORCE ---->
+        // if(nums.size() == 1){
+        //     return nums[0];
+        // }
+        // vector<int> temp;
+
+        // while(nums.size() > 1){
+        //     for(int i=0 ; i<nums.size()-1 ; i++){
+        //         int sum = nums[i]+nums[i+1];
+        //         if(sum > 9){
+        //             temp.push_back(sum%10);
+        //         }
+        //         else{
+        //             temp.push_back(sum);
+        //         }
+        //     }
+        //     nums = temp;
+        //     temp.clear();
+        // }
+        // return nums[0];
+
+        // OPTIMISED ------>
+        int n = nums.size();
+        vector<int> temp(n-1);
+
+        if(n == 1){
             return nums[0];
         }
-        vector<int> temp;
 
-        while(nums.size() > 1){
-            for(int i=0 ; i<nums.size()-1 ; i++){
-                int sum = nums[i]+nums[i+1];
-                if(sum > 9){
-                    temp.push_back(sum%10);
-                }
-                else{
-                    temp.push_back(sum);
-                }
-            }
-            nums = temp;
-            temp.clear();
+        for(int i=0 ; i<n-1 ; i++){
+            temp[i] = (nums[i]+nums[i+1])%10;
         }
-        return nums[0];
+
+        return triangularSum(temp);
     }
 };
