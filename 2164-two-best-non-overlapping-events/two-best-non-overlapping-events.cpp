0577c2a6d54386192e188775c18de47b83b34ab3
Upasana Shakya <123869989+Upasana-Shakya@users.noreply.github.com>
2025-12-23 10:22:17 +0530

    Time: 197 ms (14.29%) | Memory: 196.1 MB (18.15%) - LeetSync

--- 2164-two-best-non-overlapping-events/two-best-non-overlapping-events.cpp
+++ 2164-two-best-non-overlapping-events/two-best-non-overlapping-events.cpp
@@ -2,18 +2,30 @@ class Solution {
 public:
     int maxTwoEvents(vector<vector<int>>& events) {
         sort(events.begin(), events.end());
-        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<>> pq;
-        int maxVal = 0, ans = 0;
-
-        for (const auto& event : events) {
-            int start = event[0], end = event[1], value = event[2];
-            while (!pq.empty() && pq.top().first < start) {
-                maxVal = max(maxVal, pq.top().second);
-                pq.pop();
+        vector<vector<int>> dp(events.size(), vector<int>(3, -1));
+        return findEvents(events, 0, 0, dp);
+    }
+    // Recursive function to find the greatest sum for the pairs.
+    int findEvents(vector<vector<int>>& events, int idx, int cnt,
+                   vector<vector<int>>& dp) {
+        if (cnt == 2 || idx >= events.size()) return 0;
+        if (dp[idx][cnt] == -1) {
+            int end = events[idx][1];
+            int lo = idx + 1, hi = events.size() - 1;
+            while (lo < hi) {
+                int mid = lo + ((hi - lo) >> 1);
+                if (events[mid][0] > end)
+                    hi = mid;
+                else
+                    lo = mid + 1;
             }
-            ans = max(ans, maxVal + value);
-            pq.push({end, value});
+            int include =
+                events[idx][2] + (lo < events.size() && events[lo][0] > end
+                                      ? findEvents(events, lo, cnt + 1, dp)
+                                      : 0);
+            int exclude = findEvents(events, idx + 1, cnt, dp);
+            dp[idx][cnt] = max(include, exclude);
         }
-        return ans;
+        return dp[idx][cnt];
     }
 };
