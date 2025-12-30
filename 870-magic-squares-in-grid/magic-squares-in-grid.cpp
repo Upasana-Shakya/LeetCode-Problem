115d7571706a56548b46c40a0d980057c650d45d
Upasana Shakya <123869989+Upasana-Shakya@users.noreply.github.com>
2025-12-30 16:05:37 +0530

    Time: 0 ms (100.00%) | Memory: 11.5 MB (59.27%) - LeetSync

--- 870-magic-squares-in-grid/magic-squares-in-grid.cpp
+++ 870-magic-squares-in-grid/magic-squares-in-grid.cpp
@@ -1,42 +1,57 @@
 class Solution {
 public:
     bool isMagicSquare(vector<vector<int>>& grid, int i, int j){
-        if(i < grid.size()-1 && j < grid[i].size()-1){
-                int r0 = grid[i-1][j-1] + grid[i-1][j] + grid[i-1][j+1];
-                int r1 = grid[i][j-1] + grid[i][j] + grid[i][j+1];
-                int r2 = grid[i+1][j-1] + grid[i+1][j] + grid[i+1][j+1];
-
-                int c0 = grid[i-1][j-1] + grid[i][j-1] + grid[i+1][j-1];
-                int c1 = grid[i-1][j] + grid[i][j] + grid[i+1][j];
-                int c2 = grid[i-1][j+1] + grid[i][j+1] + grid[i+1][j+1];
-
-                int d1 = grid[i-1][j-1] + grid[i][j] + grid[i+1][j+1];
-                int d2 = grid[i-1][j+1] + grid[i][j] + grid[i+1][j-1];
-
-                int sum = r0;
-
-                if(r0 == sum && r1 == sum && r2 == sum && c0 == sum && c1 == sum && c2 == sum && d1 == sum && d2 == sum ){
-                    set<int> s({1,2,3,4,5,6,7,8,9});
-                    for(int r = 0 ; r < 3 ; r++){
-                        for(int c = 0; c < 3 ; c++){
-                            s.erase(grid[i + r - 1][j + c - 1]);
-                        }
-                    }
-                    return s.empty();
+        vector<int>v(10, 0);
+        for(int r=0 ; r<3 ; r++){
+            for(int c=0 ; c<3 ; c++){
+                int num = grid[r+i][c+j];
+                if(num < 1 || num > 9){
+                    return false;
                 }
+                v[num]++;
+            }
         }
-        return false;
+        for(int id=1 ; id<10 ; id++){
+            if(v[id] > 1){
+                return false;
+            }
+        }
+        int d1 = grid[i][j]+grid[i+1][j+1]+grid[i+2][j+2];
+        int d2 = grid[i][j+2]+grid[i+1][j+1]+grid[i+2][j];
+
+        if(d1 != d2){
+            return false;
+        }
+
+        int s1 = grid[i][j]+grid[i][j+1]+grid[i][j+2];
+        int s2 = grid[i+1][j]+grid[i+1][j+1]+grid[i+1][j+2];
+        int s3 = grid[i+2][j]+grid[i+2][j+1]+grid[i+2][j+2];
+
+        if(!(s1 == d1 && s2==d1 && s3==d1)){
+            return false;
+        }
+
+        int s4 = grid[i][j]+grid[i+1][j]+grid[i+2][j];
+        int s5 = grid[i][j+1]+grid[i+1][j+1]+grid[i+2][j+1];
+        int s6 = grid[i][j+2]+grid[i+1][j+2]+grid[i+2][j+2];
+        
+        if(!(s4 == d1 && s5==d1 && s6==d1)){
+            return false;
+        }
+        return true;
     }
     int numMagicSquaresInside(vector<vector<int>>& grid) {
-        int ans = 0;
+        int m = grid.size();
+        int n = grid[0].size();
+        int count=0;
 
-        for(int i=1 ; i<grid.size()-1 ; i++){
-            for(int j=1 ; j<grid[0].size()-1 ; j++){
+        for(int i=0 ; i<m-2 ; i++){
+            for(int j=0 ; j<n-2 ; j++){
                 if(isMagicSquare(grid, i, j)){
-                    ans++;
+                    count++;
                 }
             }
         }
-        return ans;
+        return count;
     }
 };
